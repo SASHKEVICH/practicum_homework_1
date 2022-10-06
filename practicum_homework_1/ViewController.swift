81a8331cd41c2dbfb95181432c7a14a82ecfa2f8
Aleksander Bekrenev <praise2001@yandex.ru>
2022-10-06 11:12:35 +0700

    Initial Commit

---
+++ practicum_homework_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practicum_homework_1
+//
+//  Created by Александр Бекренев on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
