a4a3b7573250860bd811fbc4f97ac9c5d6a7aae0
Aleksander Bekrenev <praise2001@yandex.ru>
2022-10-07 11:03:38 +0700

    homework done

--- practicum_homework_1/ViewController.swift
+++ practicum_homework_1/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bigButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var additionalLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        additionalLabel.textColor = .lightGray
+        
+        configureCounterLabel()
+        configureBigButton()
+        configureClearButton()
     }
 
-
+    @IBAction func increaseCounter() {
+        counter += 1
+        configureCounterLabel()
+    }
+    
+    @IBAction func clearCounter() {
+        counter = 0
+        configureCounterLabel()
+    }
+    
+    fileprivate func configureBigButton() {
+        bigButton.backgroundColor = .systemBlue
+        bigButton.tintColor = .white
+        bigButton.layer.cornerRadius = 10
+        bigButton.setTitle("Тык", for: .normal)
+        
+        /// Почему-то не меняется размер шрифта у кнопки ни здесь, ни в IB
+//        bigButton.titleLabel?.font = .systemFont(ofSize: 30, weight: .bold)
+//        bigButton.titleLabel?.adjustsFontSizeToFitWidth = true
+//        bigButton.titleLabel?.minimumScaleFactor = 0.5
+//        bigButton.titleLabel?.numberOfLines = 0
+    }
+    
+    fileprivate func configureClearButton() {
+        clearButton.backgroundColor = .systemRed
+        clearButton.tintColor = .white
+        clearButton.layer.cornerRadius = 10
+        clearButton.setTitle("Очистить", for: .normal)
+    }
+    
+    fileprivate func configureCounterLabel() {
+        counterLabel.text = "Значение счётчика: \(counter)"
+        counterLabel.sizeToFit()
+        counterLabel.center.x = self.view.center.x
+    }
 }
 
